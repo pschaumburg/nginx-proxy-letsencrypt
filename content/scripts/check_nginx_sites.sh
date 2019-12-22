e2aae53e45a7ab719710f2d5616d6c0eedd11980
Patrick Schaumburg <info@p-schaumburg.de>
2019-12-22 06:37:39 +0100

    add check_nginx_sites.sh

---
+++ content/scripts/check_nginx_sites.sh
@@ -0,0 +1,34 @@
+# !/usr/bin/env bash
+# Author: Patrick Schaumburg <info@p-schaumburg.de>
+# Script name: check_nginx_sites.sh
+
+echo "+++++++ Running nginx site check +++++++"
+
+# 1. Check, if enabled sites is still available, else remove from sites-enabled
+echo "Check for removed sites and remove them from sites-enabled"
+
+for enabledconfig in /etc/nginx/sites-enabled/*; do
+  filecheck="/etc/nginx/sites-available/$(basename $enabledconfig)"
+  if [ ! -e $filecheck ]; then
+    echo "$(basename $enabledconfig) not found anymore in sites-available, so removing it from sites-enabled."
+    rm $enabledconfig
+  fi
+done
+
+echo "Check for removed sites done"
+
+# 2. Enable sites, which are not enabled right now
+echo "Check for new sites and add them to sites-enabled"
+
+for availableconfig in '/etc/nginx/sites-available/*'; do
+    filecheck="/etc/nginx/sites-enabled/$(basename $availableconfig)"
+    if [ ! -e $filecheck ]; then
+        echo "Found $availableconfig so adding it to sites-enabled"
+        ln -s $availableconfig $filecheck
+        echo "Linked $availableconfig to $filecheck"
+    fi
+done
+
+echo "Check for new sites done"
+
+echo "+++++++ DONE nginx site check +++++++"
